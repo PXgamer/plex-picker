ffddc894a1a51862c8cf65c06c13f6a37a63900c
PXgamer <owzie123@gmail.com>
2017-01-16 14:00:01 +0000

    Finished Picker class

--- src/Picker.php
+++ src/Picker.php
@@ -4,55 +4,62 @@ namespace pxgamer\PlexPicker;
 
 /**
  * Class Picker
- * Package pxgamer\PlexPicker
+ * Package pxgamer\PlexPicker.
  */
 class Picker
 {
-	public $baseUrl;
-	
-	public $plexResponse;
-	
-	public $data = [];
-	
+    public $baseUrl;
+
+    public $plexResponse;
+
+    public $videoData;
+
+    public $data = [];
+
     public function _setBaseUrl($baseUrl)
     {
         $this->baseUrl = $baseUrl;
     }
-	
+
     public function setToken($token)
     {
         $this->data['X-Plex-Token'] = $token;
         $this->data['sort'] = 'titleSort:asc';
         $this->data['type'] = '1';
     }
-	
+
     public function setData($data)
     {
         $this->data = array_merge($this->data, $data);
     }
-	
-	public function _get()
-	{
-		$url = $this->baseUrl . '/library/sections/1/all?';
-		
-		$cu = curl_init();
+
+    public function _get()
+    {
+        $url = $this->baseUrl.'/library/sections/1/all?';
+
+        $cu = curl_init();
         curl_setopt_array(
             $cu,
             [
-                CURLOPT_URL => $url . http_build_query($this->data),
+                CURLOPT_URL => $url.http_build_query($this->data),
                 CURLOPT_SSL_VERIFYHOST => 0,
                 CURLOPT_SSL_VERIFYPEER => 0,
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_TIMEOUT => 15,
             ]
         );
-		
-		$this->plexResponse = simplexml_load_string(curl_exec($cu));
-		
+
+        $this->plexResponse = (array) simplexml_load_string(curl_exec($cu));
+
         return $this->plexResponse;
-	}
-	
-	public function chooseFilm()
-	{
-	}
+    }
+
+    public function chooseFilm()
+    {
+        $chosen = (array) $this->plexResponse['Video'][array_rand($this->plexResponse['Video'])];
+
+        $this->videoData = $chosen['@attributes'];
+
+        return $this->videoData['title'];
+    }
 }
