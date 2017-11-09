ebe3ca69ecaa3d9547573b8ce1e9dd17fbed1c02
pxgamer <owzie123@gmail.com>
2017-11-09 14:44:35 +0000

    Added namespace to the test class

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -1,13 +1,14 @@
 <?php
 
-use pxgamer\PlexPicker;
+namespace pxgamer\PlexPicker;
+
 use PHPUnit\Framework\TestCase;
 
 class MainTest extends TestCase
 {
     public function testCanBeInitialised()
     {
-        $client = new PlexPicker\Picker();
-        $this->assertInstanceOf(PlexPicker\Picker::class, $client);
+        $client = new Picker();
+        $this->assertInstanceOf(Picker::class, $client);
     }
 }
