7545d55ad6111ff5df9083a89ca26a572b64bf74
pxgamer <owzie123@gmail.com>
2017-11-09 14:58:10 +0000

    Added proper unit tests

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -6,9 +6,34 @@ use PHPUnit\Framework\TestCase;
 
 class MainTest extends TestCase
 {
-    public function testCanBeInitialised()
+    const TEST_URL = 'https://test-plex';
+    const TEST_TOKEN = 'random-string-token';
+    const TEST_DATA = [
+        'test',
+        'data',
+    ];
+
+    public function testCanSetBaseUrl()
     {
         $client = new Picker();
-        $this->assertInstanceOf(Picker::class, $client);
+        $client->setBaseUrl(self::TEST_URL);
+
+        $this->assertTrue($client->baseUrl === self::TEST_URL);
+    }
+
+    public function testCanGetCanSetToken()
+    {
+        $client = new Picker();
+        $client->setToken(self::TEST_TOKEN);
+
+        $this->assertTrue($client->data['X-Plex-Token'] === self::TEST_TOKEN);
+    }
+
+    public function testCanGetCanSetData()
+    {
+        $client = new Picker();
+        $client->setData(self::TEST_DATA);
+
+        $this->assertTrue($client->data === self::TEST_DATA);
     }
 }
