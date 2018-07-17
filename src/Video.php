589476bf9124b1a95f416f33931c85b6f8c9cf59
pxgamer <owzie123@gmail.com>
2018-07-17 23:58:17 +0100

    Add a new Video class

---
+++ src/Video.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace pxgamer\PlexPicker;
+
+/**
+ * Class Video
+ */
+class Video
+{
+    /**
+     * @var int
+     */
+    public $ratingKey;
+    /**
+     * @var string
+     */
+    public $key;
+    /**
+     * @var string
+     */
+    public $studio;
+    /**
+     * @var string
+     */
+    public $type;
+    /**
+     * @var string
+     */
+    public $title;
+    /**
+     * @var string
+     */
+    public $contentRating;
+    /**
+     * @var string
+     */
+    public $summary;
+    /**
+     * @var float
+     */
+    public $rating;
+    /**
+     * @var float
+     */
+    public $audienceRating;
+    /**
+     * @var int
+     */
+    public $year;
+    /**
+     * @var string
+     */
+    public $tagline;
+    /**
+     * @var string
+     */
+    public $thumb;
+    /**
+     * @var string
+     */
+    public $art;
+    /**
+     * @var int
+     */
+    public $duration;
+    /**
+     * @var string
+     */
+    public $originallyAvailableAt;
+    /**
+     * @var int
+     */
+    public $addedAt;
+    /**
+     * @var int
+     */
+    public $updatedAt;
+    /**
+     * @var string
+     */
+    public $audienceRatingImage;
+    /**
+     * @var string
+     */
+    public $chapterSource;
+    /**
+     * @var string
+     */
+    public $primaryExtraKey;
+    /**
+     * @var string
+     */
+    public $ratingImage;
+}
